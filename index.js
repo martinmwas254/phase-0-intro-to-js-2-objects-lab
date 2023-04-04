df22dbb18ff61421fa533c4666a57fe519c371ba
martinmwas254 <martinmwas264@gmail.com>
2023-04-04 15:01:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+const employee = {
+  name: "Sam  Smith",
+  streetAddress: "Nairobi",
+};
+
+// Function to update an employee object with a new key-value pair
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value,
+  };
+}
+
+// updates `employee` with the given `key` and `value` (it is destructive) and returns the entire updated employee
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+// deletes `key` from a clone of an object and returns a new object (it is non-destructive)
+
+function deleteFromEmployeeByKey(obj, key) {
+  const newObj = { ...obj };
+  delete newObj[key];
+  return newObj;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
